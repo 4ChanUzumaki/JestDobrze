90e61be13f6ccad36834c80c7b6b353496cbea2a
4ChanUzumaki <natalia.derman.05@wp.pl>
2022-04-01 09:00:14 +0200

    update

--- Algorytmliczbpierwszych/Algorytmliczbpierwszych.cpp
+++ Algorytmliczbpierwszych/Algorytmliczbpierwszych.cpp
@@ -1,7 +1,35 @@
-﻿
-#include <iostream>
+﻿#include<iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i;
+	int	j;
+	int	k = 0;
+	int wartosc = 0;
+	int	obliczenia = 1;
+
+	cout << "-----------------------------" << endl;
+	cout << "Podaj liczbe liczb pierwszych" << endl;
+	cout << "-----------------------------" << endl;
+	cin >> wartosc;
+
+	for (i = 3; i > 0; i++)
+	{
+		for (j = 2; j <= i / 2; j++)
+		{
+			if (i % j == 0) {
+				k = 1;
+				break;
+			}
+		}
+			if (k == 0) {
+				cout << i << endl;
+				obliczenia++;
+			}
+				k = 0;
+				if (obliczenia == wartosc + 1)
+					break;
+	}
+	return 0;
 }
